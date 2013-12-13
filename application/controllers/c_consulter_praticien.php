3b635a03724385bfe5f844c58007a83a0be3b633
Alexx <alexis.anctil@gmail.com>
2013-12-13 12:25:00 +0100

    Test nouveau dossier

--- application/controllers/c_consulter_praticien.php
+++ application/controllers/c_consulter_praticien.php
@@ -3,7 +3,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class c_consulter_praticien extends CI_Controller {
 	function index() {
-		$this->load->view("v_consulter_praticien");
+		$this->load->view("v_consulter_praticien.php");
 	}
 }
 /*function consulter() {
