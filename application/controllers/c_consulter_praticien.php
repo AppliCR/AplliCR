a7cced138c6c77ee98a001f7467388dec6c409a0
Alexx <alexis.anctil@gmail.com>
2013-12-13 12:19:46 +0100

    Test CSSAlexis

--- application/controllers/c_consulter_praticien.php
+++ application/controllers/c_consulter_praticien.php
@@ -1,4 +1,11 @@
 <?php
-function consulter() {
-	
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class c_consulter_praticien extends CI_Controller {
+	function index() {
+		$this->load->view("v_consulter_praticien");
+	}
 }
+/*function consulter() {
+	
+}*/
---
+++ application/views/v_consulter_praticien.php
@@ -0,0 +1,16 @@
+<?php
+$this->load->helper("html");
+echo doctype('html4-frame'); 
+echo meta('Content-Type', 'text/html; charset=Cp1252');
+echo link_tag('./style/AppliCRAlexis.css');
+?>
+<body>
+<!-- Commentaire -->
+	<div class="Fond_site">
+		<div class="Couleur_titre">
+			<h1>Praticiens</h1>
+		</div>	
+</div>
+</body>
+
+	
